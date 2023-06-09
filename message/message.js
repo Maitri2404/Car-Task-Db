0ff8b9e40a10aabcda0647d9bb61cb54f4c1f891
Maitri2404 <maitri.trivedi@yudiz.com>
2023-06-09 15:13:59 +0530

    test commit

---
+++ message/message.js
@@ -0,0 +1,6 @@
+const message = {
+    carNotFound: {message: 'Car not found'},
+    serverError: {error: 'Internal server error'},
+    brandNotFound: {message: 'Brand not found'},
+    checkSameCity: {message: 'Seller and user are not from the same city'}
+}
